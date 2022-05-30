975ae8491b3dd689b176018281b366f74478200a
Maikol Guzman <mikeguzman@gmail.com>
2022-05-29 19:11:44 -0600

    More comments

--- app/src/main/java/edu/mike/frontend/taskapp/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/edu/mike/frontend/taskapp/viewmodel/ViewModelFactory.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import edu.mike.frontend.taskapp.repository.MainRepository
 
+/**
+ * if we need to pass some input data to the constructor of the viewModel,
+ * we need to create a factory class for viewModel.
+ */
 class ViewModelFactory constructor(
     private val repository: MainRepository
 ) : ViewModelProvider.Factory {
