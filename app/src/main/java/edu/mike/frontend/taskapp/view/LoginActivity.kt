96d4e88442c2d96d8c4d6af82a172c5b6c569adf
Maikol Guzman <mikeguzman@gmail.com>
2022-06-03 11:06:31 -0600

    About and logout from activity

--- app/src/main/java/edu/mike/frontend/taskapp/view/LoginActivity.kt
+++ app/src/main/java/edu/mike/frontend/taskapp/view/LoginActivity.kt
@@ -26,8 +26,6 @@ class LoginActivity : AppCompatActivity() {
     // Definition of the binding variable
     private lateinit var binding: ActivityLoginBinding
     private lateinit var loginViewModel: LoginViewModel
-    private lateinit var sessionManager: SessionManager
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +33,6 @@ class LoginActivity : AppCompatActivity() {
         // With View Binding
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        sessionManager = SessionManager(this)
 
         // LoginViewModelFactory
         loginViewModel =
