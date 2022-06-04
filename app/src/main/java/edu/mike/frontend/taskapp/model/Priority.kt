eefc6a81b3f178c300a0de6f8efa3628f8c8c215
Maikol Guzman <mikeguzman@gmail.com>
2022-06-03 23:47:20 -0600

    Spinner functionality

--- app/src/main/java/edu/mike/frontend/taskapp/model/Priority.kt
+++ app/src/main/java/edu/mike/frontend/taskapp/model/Priority.kt
@@ -3,4 +3,8 @@ package edu.mike.frontend.taskapp.model
 data class Priority(
     var id: Long? = null,
     var label: String? = null,
-)
+) {
+    override fun toString(): String {
+        return "$label"
+    }
+}
--- app/src/main/java/edu/mike/frontend/taskapp/view/task/TaskAdd.kt
+++ app/src/main/java/edu/mike/frontend/taskapp/view/task/TaskAdd.kt
@@ -1,36 +1,39 @@
 package edu.mike.frontend.taskapp.view.task
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.SpinnerAdapter
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import edu.mike.frontend.taskapp.R
 import edu.mike.frontend.taskapp.databinding.FragmentTaskAddBinding
+import edu.mike.frontend.taskapp.model.Priority
 import edu.mike.frontend.taskapp.viewmodel.TaskViewModel
 import edu.mike.frontend.taskapp.viewmodel.TaskViewModelFactory
 
 class TaskAdd : Fragment() {
 
     // Definition of the binding variable
-    private var _binding: FragmentTaskAddBinding ?= null
+    private var _binding: FragmentTaskAddBinding? = null
     private val binding get() = _binding!!
 
     // View model
     private lateinit var taskViewModel: TaskViewModel
-
-    private val languages = arrayOf("High", "Medium", "Low")
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        var priorities = ArrayList<Priority>()
+
+        priorities.add(Priority(id = 1, label = "Height"))
+        priorities.add(Priority(id = 2, label = "Medium"))
+        priorities.add(Priority(id = 3, label = "Low"))
+
         _binding = FragmentTaskAddBinding.inflate(inflater, container, false)
 
         // TaskViewModelFactory
@@ -39,15 +42,21 @@ class TaskAdd : Fragment() {
 
         // access the spinner
         if (binding.spinnerPriority != null) {
-            val adapter = ArrayAdapter(activity?.applicationContext!!, android.R.layout.simple_spinner_item, languages)
+            val adapter : ArrayAdapter<Priority> = ArrayAdapter<Priority>(
+                activity?.applicationContext!!,
+                android.R.layout.simple_spinner_item,
+                priorities
+            )
 
             binding.spinnerPriority.adapter = adapter
 
             binding.spinnerPriority.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>,
-                                            view: View, position: Int, id: Long) {
-
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    val prioritySelected : Priority = parent.selectedItem as Priority
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>) {
